29de04a3a9e1113890e81e30e42865aafae5f355
usssagn1 <zaytsevdima031107@gmail.com>
2024-03-16 15:50:11 +0200

    CRUD (simply)

--- src/Routing/HeadPhonesRouter.ts
+++ src/Routing/HeadPhonesRouter.ts
@@ -7,9 +7,44 @@ export const getHeadphonesRouter = () => {
     const headphonesRouter = express.Router();
 
     headphonesRouter.get('/', async (req, res) => {
-        const headphones = headphonesRepository.findHeadphones(req.query.title);
+        const headphones = headphonesRepository.findHeadphone(req.query.title);
         const foundHeadphones = await headphones
         res.status(HTTP_STATUSES.OK_200).send(foundHeadphones)
+    })
+
+    headphonesRouter.get('/:name', async (req, res) => {
+        const foundHeadphone = await headphonesRepository.findHeadphoneByID(req.params.name);
+        if (!foundHeadphone) {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+            return;
+        }
+
+        res.json(foundHeadphone);
+    })
+
+    headphonesRouter.post('/', async (req, res) => {
+        const createdHeadphone = await headphonesRepository.createHeadphone(req.body.name);
+        return res.status(HTTP_STATUSES.CREATED_201).json(createdHeadphone)
+    })
+
+    headphonesRouter.put('/:name', async (req, res) => {
+        try {
+            if (!req.body || Object.keys(req.body).length === 0) {
+                return res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+            }
+
+            const updateResult = await headphonesRepository.updateHeadphone(req.params.name, req.body);
+
+            res.sendStatus(updateResult.status)
+        } catch (e) {
+            console.error("Error updating course:", e);
+            res.sendStatus(HTTP_STATUSES.INTERNAL_SERVER_ERROR_500);
+        }
+    })
+
+    headphonesRouter.delete('/:name', async (req, res ) => {
+        const isDeleted = await headphonesRepository.deleteHeadphone(req.params.name)
+        isDeleted ? res.sendStatus(HTTP_STATUSES.NO_CONTENT_204) : res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
     })
 
     return headphonesRouter;
--- src/repositories/headphonesRepoDB.ts
+++ src/repositories/headphonesRepoDB.ts
@@ -1,8 +1,9 @@
 import {HeadphoneCollection} from "./dataBase";
 import {HeadphoneType} from "../DBSettings/dbSettings";
+import {HTTP_STATUSES} from "../utils";
 
 export const headphonesRepository = {
-    async findHeadphones(title: any): Promise<HeadphoneType[]> {
+    async findHeadphone(title: any): Promise<HeadphoneType[]> {
         const filter: any = {}
         if(title) {
             filter.title = {$regex: title}
@@ -10,5 +11,24 @@ export const headphonesRepository = {
 
         return HeadphoneCollection.find(filter).toArray()
     },
-
+    async findHeadphoneByID(name: string) {
+        let headphones = await HeadphoneCollection.findOne({name: name})
+        return headphones
+    },
+    async createHeadphone(createdHeadphone: HeadphoneType) {
+        await HeadphoneCollection.insertOne(createdHeadphone)
+        return createdHeadphone;
+    },
+    async updateHeadphone(name: string, updateParams: Partial<HeadphoneType>) {
+        const result = await HeadphoneCollection.updateOne({name: name}, { $set: updateParams})
+        if(result.matchedCount) {
+            return { status: HTTP_STATUSES.NO_CONTENT_204}
+        } else {
+            return { status: HTTP_STATUSES.NOT_FOUND_404}
+        }
+    },
+    async deleteHeadphone(name: string) {
+        const result = await HeadphoneCollection.deleteOne({name: name})
+        return result.deletedCount === 1;
+    }
 }
