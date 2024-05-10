29d552e3537e6e558746ee06c999307f24291c3e
usssagn1 <zaytsevdima031107@gmail.com>
2024-05-10 14:52:49 +0300

    package

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,20 @@ import {app} from "./app";
 import {startDB} from "./repositories/dataBase";
 import dotenv from 'dotenv'
 
+const cors = require("cors");
+
 dotenv.config()
 
 console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 const startApp = async () => {
     await startDB();
+    app.use(cors())
     app.listen(PORT, () => {
         console.log(`Example app listening on port: http://localhost:${PORT}`)
     })
 }
 
-startApp()
+startApp();
